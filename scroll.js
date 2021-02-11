fddef35fda36e01c6bed2d6fda1a6759ac7bb91d
Skane-Gahi <73883920+Skane-Gahi@users.noreply.github.com>
2021-02-11 15:43:43 +0100

    contact form and poslishing

--- scroll.js
+++ scroll.js
@@ -1,6 +1,6 @@
 
-/*
-const container = document.getElementsByClassName('Modal');
+
+const container = document.getElementsByClassName('OpenModal');
 const html = document.querySelector('html');
 
 const closeBtn = document.querySelectorAll('.ModalClose');
@@ -14,15 +14,4 @@ for (let i = 0; i < closeBtn.length; i++) {
     closeBtn[i].addEventListener('click', ()=>{html.classList.toggle('active');
 })}
 
-*/
-var btn = document.getElementsByClassName("OpenModal");
-var btn2 = document.getElementsByClassName("ModalClose");
 
-btn.onclick = function() {
-    document.body.style.overflow = "hidden"; // ADD THIS LINE
-}
-
-// When the user clicks on <span> (x), close the modal
-btn2.onclick = function() {
-    document.body.style.overflow = "auto"; // ADD THIS LINE
-}
