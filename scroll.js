45bf522197b41afcfb1b06371dbe092d36006d0a
Skane-Gahi <73883920+Skane-Gahi@users.noreply.github.com>
2021-02-15 14:06:29 +0100

    Modal scroll

--- scroll.js
+++ scroll.js
@@ -1,17 +1,25 @@
-
-
-const container = document.getElementsByClassName('OpenModal');
-const html = document.querySelector('html');
-
-const closeBtn = document.querySelectorAll('.ModalClose');
+const container = document.getElementsByClassName("OpenModal");
+const closeBtn = document.getElementsByClassName("ModalClose");
 
 for (let i = 0; i < container.length; i++) {
-
-    container[i].addEventListener('click', ()=>{html.classList.toggle('active');
+    container[i].addEventListener('click', ()=>{document.body.style.overflow = "hidden";
 })}
 
 for (let i = 0; i < closeBtn.length; i++) {
-    closeBtn[i].addEventListener('click', ()=>{html.classList.toggle('active');
+    closeBtn[i].addEventListener('click', ()=>{document.body.style.overflow = "auto";
 })}
 
+/*
+container.onclick = function() {
+    modal.style.display = "block";
+    document.body.style.overflow = "hidden"; // ADD THIS LINE
+    document.body.style.height = "100%"; // ADD THIS LINE
+}
 
+// When the user clicks on <span> (x), close the modal
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+    document.body.style.overflow = "auto"; // ADD THIS LINE
+    document.body.style.height = "auto"; // ADD THIS LINE
+}
+*/
